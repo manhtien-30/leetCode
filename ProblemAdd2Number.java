4bbf9b6bb6c9103f8bee96e8271d64b1eba51a7f
manhtien-30 <tien310001@gmail.com>
2024-07-28 21:08:57 +0700

    problem 1: add two numbers

---
+++ ProblemAdd2Number.java
@@ -0,0 +1,37 @@
+/**
+ * ProblemAdd2Number
+ */
+public class ProblemAdd2Number {
+  class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+  
+  public int revertOrderNumber(int number) {
+    return 0;
+  }
+  public int getIndexFromListNode(ListNode list) {
+    return 0;
+  }
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    return null;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
